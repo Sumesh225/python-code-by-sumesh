5c0aee3c78c4a91bc373affe982a6b78a97feb65
Sumesh225 <146158501+Sumesh225@users.noreply.github.com>
2023-09-29 11:43:25 +0530

    2nd day codes

---
+++ pyhon.py
@@ -0,0 +1,14 @@
+print(100)
+print("hey there")
+print("hello jecrc")
+print(23+100)
+x=100
+a=4.6
+z=("hello there")
+print(x,a,z)
+#printing data type of variable
+print(type(z))
+#maths operation
+sum1=x+a
+mul1=x+a
+print(sum1,mul1)
---
+++ task1.py
@@ -0,0 +1,5 @@
+num1=input("enter the 1st number")
+num2=input("enter second number")
+sum=int(num1)+int(num2)
+mul=int(num1)+int(num2)
+print(sum,mul)
